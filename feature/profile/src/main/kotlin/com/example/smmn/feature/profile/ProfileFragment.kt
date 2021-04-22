914d646d2637e82f1f0c8d550fb5a116ff9f6afe
szhukov <s.zhukov@cft.ru>
2021-04-22 16:23:55 +0700

    init

--- feature/profile/src/main/kotlin/com/example/smmn/feature/profile/ProfileFragment.kt
+++ feature/profile/src/main/kotlin/com/example/smmn/feature/profile/ProfileFragment.kt
@@ -3,6 +3,7 @@ package com.example.smmn.feature.profile
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.example.smmn.shared.model.Info
 import com.example.smmn.shared.navigation.navigate
 import kotlinx.android.synthetic.main.fragment_profile.*
 
@@ -12,7 +13,7 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
         super.onViewCreated(view, savedInstanceState)
 
         buttonToInfo.setOnClickListener {
-//            navigate()
+            navigate(R.id.action_profileFragment_to_infoFragment, data = Info("name", "surname"))
         }
     }
 }
